d2d06703552288494102442dad7f95d162ec7ace
Usman Zaheer <usmanzaheer1995@gmail.com>
2026-02-20 23:11:30 +0100

    update configs. add env file. update CI workflows

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    let configuration = get_configuration().expect("failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = tokio::net::TcpListener::bind(address).await?;
     run(listener).await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
+use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use sqlx::{Connection, PgConnection};
 use tokio::net::TcpListener;
 
 #[tokio::test]
@@ -19,9 +21,18 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn subscribe_returns_200_for_valid_form_data() {
+    // Arrange
     let app_address = spawn_app().await;
+    let configuration = get_configuration().expect("failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("failed to connect to database");
+
     let client = reqwest::Client::new();
 
+    // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
@@ -31,7 +42,16 @@ async fn subscribe_returns_200_for_valid_form_data() {
         .await
         .expect("Failed to send subscription request");
 
-    assert_eq!(200, response.status().as_u16())
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "ursula_le_guin@gmail.com");
 }
 
 #[tokio::test]
